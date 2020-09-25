0d8d9c1375193364a4d39ce5c303223259dd916d
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-09-25 17:28:21 -0300

    Não sei se deveria fazer isso mas eu removi as integridades dos bootstraps da vida pois estava dando erro

--- src/default/bottonPage.php
+++ src/default/bottonPage.php
@@ -1,8 +1,8 @@
 
-<script src="/bootstrap/script.js"></script>
-<script src="/bootstrap/js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="/bootstrap/js/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="/bootstrap/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="/script.js"></script>
+<script src="/bootstrap/js/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="/bootstrap/js/popper.min.js" crossorigin="anonymous"></script>
+<script src="/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
