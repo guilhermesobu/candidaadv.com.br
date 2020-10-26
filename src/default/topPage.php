c3858e2b78f1e58c878702d202312e43b4a335b5
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-10-26 09:27:20 -0300

    Adicionado Título

--- src/default/topPage.php
+++ src/default/topPage.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    <title>Candida Advocacia</title>
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="/favicon4.ico" />
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
