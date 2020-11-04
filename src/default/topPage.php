dd1af04cb2a8ed54ecf5151dc6f7e9d9c0089c28
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-11-04 17:37:30 -0300

    Deu ruim no direcionamento para SSL (https)

--- src/default/topPage.php
+++ src/default/topPage.php
@@ -6,7 +6,7 @@
     <title>Candida Advocacia</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="/favicon4.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="/scss/index.css">
 </head>
