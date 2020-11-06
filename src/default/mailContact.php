94d9fa0a975b9fbbf855293ec5438dc7144f81d4
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-11-06 13:34:00 -0300

    Correção 4, modal, não deuc erto o modal, coloquei um alert simples

--- src/default/mailContact.php
+++ src/default/mailContact.php
@@ -60,23 +60,19 @@ if (!isset($_POST["eMail"])) {
         //Para utilizar a funcao header nao pode haver nenhum dado enviado antes do header
         //Redirecionamento por META tag:
         echo "<script>
-        location='https://www.candidaadv.com.br/'
         var mailModalJs = document.querySelector('#mailModalBtn');
             setTimeout(()=>{mailModalJs.click();}, 1);
             console.log('Mensagem de sucesso mostrada.');
+            alert('Seu emaul foi enviado');
         </script>";
 
-        //echo '<meta http-equiv="refresh" content="0;url=https://www.candidaadv.com.br/" />';
+        echo '<meta http-equiv="refresh" content="0;url=https://www.candidaadv.com.br/" />';
+        
         //Redirecionamento por JavaScript: 
         //echo " <script> location='http://www.seudominio.com.br/' </script> "; 
     } else {
         // sending failed, display error message
         echo "<p>Seu e-mail nao p&ocirc;de ser enviado.</p>";
-        echo "<script>
-        var mailModalJs = document.querySelector('#mailModalBtn');
-            setTimeout(()=>{mailModalJs.click();}, 1);
-            console.log('Mensagem de sucesso mostrada.');
-        </script>";
     }
 } //else
 ?>
