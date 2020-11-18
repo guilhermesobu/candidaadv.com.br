24732e9d138d456878d8e57e2bda6fbf5d1b83ca
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-11-18 18:56:08 -0300

    Para passar pro note

--- src/default/socialMedia.php
+++ src/default/socialMedia.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-fill p-2 mx-1">
             <a href="https://api.whatsapp.com/send?phone=5519993072185">
-                <img class="bg-wine rounded-circle" style="height: 55px;" src="/src/img/icons/icone-whatsapp.png">
+                <img class="bg-wine" style="height: 55px;" src="/src/img/icons/Whats.png">
             </a>
         </div>
         <div class="col-fill p-2 mx-1">
