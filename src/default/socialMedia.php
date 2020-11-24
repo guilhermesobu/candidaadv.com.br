21eea5dad7cdda63fd6c2d4ca7765b830a2424c6
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-11-24 08:12:10 -0300

    Consertado o icone do whats

--- src/default/socialMedia.php
+++ src/default/socialMedia.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-fill p-2 mx-1">
             <a href="https://api.whatsapp.com/send?phone=5519993072185">
-                <img class="bg-wine" style="height: 55px;" src="/src/img/icons/Whats-Wine.png">
+                <img class="" style="height: 55px;" src="/src/img/icons/Whats-Wine.png">
             </a>
         </div>
         <div class="col-fill p-2 mx-1">
