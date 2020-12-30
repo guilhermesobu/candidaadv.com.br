c0f4bc329bef01ffb7fad9fbda7e9c61494bfb28
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-12-30 11:23:51 -0300

    x

--- src/default/topPage.php
+++ src/default/topPage.php
@@ -3,12 +3,10 @@
 
 <head>
 
-
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-24FTBQ023N"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-
         function gtag() {
             dataLayer.push(arguments);
         }
@@ -16,23 +14,20 @@
         gtag('config', 'G-24FTBQ023N');
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-
     <!-- Métdo de verificação do GOOGLE -->
     <meta name="google-site-verification" content="QKHWHlc-ZpoJc1mTAPEbfCgsOkR995A7RCKkqRfqb7I" />
 
-
     <meta charset="UTF-8">
     <title>Candida Advocacia</title>
-
     <meta name="keywords" content="Naiara Candida de Lima, Advocacia, Previdência, Família e Sucessões, Direito">
     <meta name="description" content="Avogada de Direito de Família e Sucessões e Previdenciário">
     <meta name="author" content="Naiara Candida de Lima">
-
-
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="/scss/index.css">
+
 </head>
 
 <body class="david">
